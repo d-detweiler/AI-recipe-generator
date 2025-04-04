e6a985149182ae97cc6eb169367df6702dda2674
d-detweiler <d.s.detweiler@gmail.com>
2025-04-04 15:31:51 -0700

    Added basic form handling and added Typewriter plugin

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "the recipe",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator");
+recipeFormElement.addEventListener("submit", generateRecipe);
